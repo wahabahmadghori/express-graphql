ee44b7c9e59ac8d55c61b11fb574d7c922faed2a
Wahab Ahmad <wahabahmadghori@gmail.com>
2023-08-16 20:46:12 +0500

    /graphql

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const {} = require('express-graphql')
+const {graphqlHTTP} = require('express-graphql')
 
 const app = express()
+
+app.use('/graphql', graphqlHTTP({schema:{}, graphiql: true}))
 
 app.listen(5000, ()=>{
     console.log('server is running at http://localhost:5000/')
